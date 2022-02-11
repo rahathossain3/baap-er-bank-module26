7cbaddbbe57c23a2b9de2d79852c929d8a08bcb6
rahathossain3 <rahathossain210@gmail.com>
2022-02-11 17:49:32 +0600

    bank page

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,21 @@
 // headle deposit button event by
+
 //first get button
 document.getElementById('deposit-button').addEventListener('click', function () {
-    console.log('ok');
+
+    //get  the amount deposited
+    // get input field------- 
+    const depositInput = document.getElementById('deposit-input');
+    //get input value---------
+    const depositAmmount = depositInput.value;
+    console.log(depositAmmount);
+
+    // whice position value show
+    const depositTatal = document.getElementById('deposit-total');
+    // value set
+    depositTatal.innerText = depositAmmount;
+
+    //clear the deposit input field
+    depositInput.value = '';
+
 })
